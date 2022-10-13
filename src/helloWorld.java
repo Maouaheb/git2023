6a55eac4fe2f643c1b4f8cc8b77f8447d735d3d1
Maweheb Belarbi <maouaheb.belarbi@unamur.be>
2022-10-13 16:07:01 +0200

    soustraction somme multiple

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,12 +1,23 @@
 public class helloWorld {
-    public int  multiply(int a, int b){
+    public static void main(String args[]){
+        int a=10;
+        int b=17;
+        System.out.println("la somme est "+somme(a,b)+" la soustraction "+soustraction(a,b)+" multiplication "+multiply(a,b));
+    }
+    public static int  multiply(int a, int b){
         return a*b;
     }
-    public int  multiply2(int a, int b){
-        return a*b;
+    public static int  somme(int a, int b){
+        return a+b;
     }
-    public int  multiply3(int a, int b){
-        return a*b;
+    public static int  soustraction(int a, int b){
+        if(a>b){
+            return a-b;
+        }
+        else{
+            return b-a;
+
+        }
     }
 
 }
