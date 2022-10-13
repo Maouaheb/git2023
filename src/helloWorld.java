1d3ac74f820196e0af1727902cedf6b97753b97b
Maweheb Belarbi <maouaheb.belarbi@unamur.be>
2022-10-13 15:35:59 +0200

    123

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -2,4 +2,11 @@ public class helloWorld {
     public int  multiply(int a, int b){
         return a*b;
     }
+    public int  multiply2(int a, int b){
+        return a*b;
+    }
+    public int  multiply3(int a, int b){
+        return a*b;
+    }
+
 }
